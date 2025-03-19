19d8019b4c7ad4a04a0360fb6aecfc7807803ee6
olegus199 <okremnyov@yandex.ru>
2025-03-19 06:16:57 +0300

    feat: add Menu

--- src/common/Main.tsx
+++ src/common/Main.tsx
@@ -1,12 +1,14 @@
 import styles from "./Main.module.scss";
 import { FC } from "react";
 import Sidebar from "./Sidebar.tsx";
+import Menu from "./Menu.tsx";
 
 const Main: FC = () => {
   return (
     <main className={styles.main}>
       <aside className={styles.sidemenu}>
         <Sidebar />
+        <Menu />
       </aside>
       <section className={styles.main_content}></section>
     </main>
---
+++ src/common/Menu.tsx
@@ -0,0 +1,57 @@
+import styles from "./Menu.module.scss";
+import { FC } from "react";
+import SearchInput from "../UI/SearchInput.tsx";
+import { LuFilter } from "react-icons/lu";
+import ButtonBorder from "../UI/ButtonBorder.tsx";
+import { RiUnpinLine } from "react-icons/ri";
+import { FaChevronRight } from "react-icons/fa6";
+
+const menuItems: string[] = [
+  "Моя работа",
+  "Структура портала",
+  "Личное расписание",
+  "Отсутствие на рабочем месте",
+  "Портфель услуг",
+  "Дашборды",
+  "Доски задач",
+  "Обращения",
+  "События",
+  "Инциденты",
+  "Проблемы",
+  "Настройка каталогов",
+  "Запросы на обслуживание",
+  "Запросы на изменение",
+  "Управление конфигурациями",
+  "Управление уровнем услуг",
+  "Настройка соответствий",
+];
+
+const Menu: FC = () => {
+  return (
+    <div className={styles.menu}>
+      <div className={styles.search_container}>
+        <SearchInput
+          Icon={LuFilter}
+          placeholder="Поиск по меню"
+        />
+        <ButtonBorder maxWidth="fit-content">
+          <RiUnpinLine className={styles.unpin_icon} />
+        </ButtonBorder>
+      </div>
+
+      <ul className={styles.menu_itmes_list}>
+        {menuItems.map((item, idx) => (
+          <li
+            key={idx}
+            className={styles.menu_item}
+          >
+            <FaChevronRight className={styles.chevron} />
+            <p>{item}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
